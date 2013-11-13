312982a2947deb94213b306c7efd8bbb26979d00
Alex Paulau <lacus.soli@gmail.com>
2013-11-13 16:38:45 +0300

    Update lab03_2.sql
    final

--- lab03.2/lab03_2.sql
+++ lab03.2/lab03_2.sql
@@ -1,21 +1,21 @@
-﻿--3.2
+--3.2
 
---  ВЛОЖЕННЫЕ ПОДЗАПРОСЫ:
---  ПОДЗАПРОСЫ, ВЫБИРАЮЩИЕ ОДНУ СТРОКУ
---   1. Найти имена сотрудников, получивших за годы начисления зарплаты минимальную зарплату.
-SELECT E.EMPNAME 
+-- ВЛОЖЕННЫЕ ПОДЗАПРОСЫ:
+-- ПОДЗАПРОСЫ, ВЫБИРАЮЩИЕ ОДНУ СТРОКУ
+-- 1. Найти имена сотрудников, получивших за годы начисления зарплаты минимальную зарплату.
+SELECT E.EMPNAME
     FROM EMP E
     WHERE (
-    SELECT MIN(S.SALVALUE) 
-        FROM SALARY S 
-        WHERE S.EMPNO = E.EMPNO) 
+    SELECT MIN(S.SALVALUE)
+        FROM SALARY S
+        WHERE S.EMPNO = E.EMPNO)
     IN (
-    SELECT J.MINSALARY 
-        FROM JOB J NATURAL JOIN CAREER C 
+    SELECT J.MINSALARY
+        FROM JOB J NATURAL JOIN CAREER C
         WHERE E.EMPNO = C.EMPNO);
 
---  ПОДЗАПРОСЫ, ВОЗВРАЩАЮЩИЕ БОЛЕЕ ОДНОЙ СТРОКИ
---   2. Найти имена сотрудников, работавших или работающих в тех же отделах, в которых работал или работает сотрудник с именем RICHARD MARTIN.
+-- ПОДЗАПРОСЫ, ВОЗВРАЩАЮЩИЕ БОЛЕЕ ОДНОЙ СТРОКИ
+-- 2. Найти имена сотрудников, работавших или работающих в тех же отделах, в которых работал или работает сотрудник с именем RICHARD MARTIN.
 SELECT E.EMPNAME
     FROM EMP E NATURAL JOIN CAREER C
     WHERE C.DEPTNO IN (
@@ -23,21 +23,17 @@ SELECT E.EMPNAME
         FROM CAREER C NATURAL JOIN EMP E
         WHERE E.EMPNAME='RICHARD MARTIN');
 
---  СРАВНЕНИЕ БОЛЕЕ ЧЕМ ПО ОДНОМУ ЗНАЧЕНИЮ
---   3. Найти имена сотрудников, работавших или работающих в тех же отделах и должностях, что и сотрудник 'RICHARD MARTIN'.
+-- СРАВНЕНИЕ БОЛЕЕ ЧЕМ ПО ОДНОМУ ЗНАЧЕНИЮ
+-- 3. Найти имена сотрудников, работавших или работающих в тех же отделах и должностях, что и сотрудник 'RICHARD MARTIN'.
 SELECT E.EMPNAME
     FROM EMP E NATURAL JOIN CAREER C
-    WHERE C.DEPTNO IN (
-    SELECT C.DEPTNO
-        FROM CAREER C NATURAL JOIN EMP E
-        WHERE E.EMPNAME='RICHARD MARTIN')
-    AND C.JOBNO IN (
-    SELECT C.JOBNO
+    WHERE (C.DEPTNO, C.JOBNO) IN (
+    SELECT C.DEPTNO, C.JOBNO
         FROM CAREER C NATURAL JOIN EMP E
         WHERE E.EMPNAME='RICHARD MARTIN');
 
---  ОПЕРАТОРЫ ANY/ALL
---   4. Найти сведения о номерах сотрудников, получивших за какой-либо месяц зарплату большую чем средняя зарплата за 2000 г. или большую чем средняя зарплата за 2001г.
+-- ОПЕРАТОРЫ ANY/ALL
+-- 4. Найти сведения о номерах сотрудников, получивших за какой-либо месяц зарплату большую чем средняя зарплата за 2000 г. или большую чем средняя зарплата за 2001г.
 SELECT EMPNO
     FROM SALARY
     WHERE (
@@ -49,15 +45,15 @@ SELECT EMPNO
         FROM SALARY
         WHERE YEAR = 2001) < ANY(SALVALUE);
 
---   5. Найти сведения о номерах сотрудников, получивших зарплату за какой-либо месяц большую чем среднии зарплаты за все годы начислений.
+-- 5. Найти сведения о номерах сотрудников, получивших зарплату за какой-либо месяц большую чем среднии зарплаты за все годы начислений.
 SELECT EMPNO
     FROM SALARY
     WHERE (
     SELECT AVG(SALVALUE)
         FROM SALARY) < ANY(SALVALUE);
 
---  ИСПОЛЬЗОВАНИЕ HAVING С ВЛОЖЕННЫМИ ПОДЗАПРОСАМИ
---   6. Определить годы, в которые начисленная средняя зарплата была больше средней зарплаты за все годы начислений.
+-- ИСПОЛЬЗОВАНИЕ HAVING С ВЛОЖЕННЫМИ ПОДЗАПРОСАМИ
+-- 6. Определить годы, в которые начисленная средняя зарплата была больше средней зарплаты за все годы начислений.
 SELECT YEAR
     FROM SALARY
     GROUP BY YEAR
@@ -65,8 +61,8 @@ SELECT YEAR
     SELECT AVG(SALVALUE)
         FROM SALARY);
 
---  КОРРЕЛИРУЮЩИЕ ПОДЗАПРОСЫ
---   7. Определить номера отделов, в которых работали или работают сотрудники, имеющие начисления зарплаты.
+-- КОРРЕЛИРУЮЩИЕ ПОДЗАПРОСЫ
+-- 7. Определить номера отделов, в которых работали или работают сотрудники, имеющие начисления зарплаты.
 SELECT DEPTNO
     FROM DEPT D
     WHERE DEPTNO IN (
@@ -76,8 +72,8 @@ SELECT DEPTNO
     GROUP BY DEPTNO
     ORDER BY DEPTNO;
 
---  ОПЕРАТОР EXISTS
---   8. Определить номера отделов, в которых работали или работают сотрудники, имеющие начисления зарплаты.
+-- ОПЕРАТОР EXISTS
+-- 8. Определить номера отделов, в которых работали или работают сотрудники, имеющие начисления зарплаты.
 SELECT DEPTNO
     FROM DEPT D
     WHERE EXISTS (
@@ -86,8 +82,8 @@ SELECT DEPTNO
         WHERE D.DEPTNO = C.DEPTNO)
     ORDER BY DEPTNO;
 
---  ОПЕРАТОР NOT EXISTS
---   9. Определить номера отделов, для сотрудников которых не начислялась зарплата.
+-- ОПЕРАТОР NOT EXISTS
+-- 9. Определить номера отделов, для сотрудников которых не начислялась зарплата.
 SELECT DEPTNO
     FROM DEPT D
     WHERE NOT EXISTS (
@@ -96,21 +92,21 @@ SELECT DEPTNO
         WHERE D.DEPTNO = C.DEPTNO)
     ORDER BY DEPTNO;
 
---  СОСТАВНЫЕ ЗАПРОСЫ
---   10. Вывести сведения о карьере сотрудников с указанием названий и адресов отделов вместо номеров отделов.
+-- СОСТАВНЫЕ ЗАПРОСЫ
+-- 10. Вывести сведения о карьере сотрудников с указанием названий и адресов отделов вместо номеров отделов.
 SELECT E.EMPNAME, D.DEPTNAME, D.DEPTADDR
     FROM EMP E NATURAL JOIN CAREER C NATURAL JOIN DEPT D
     ORDER BY E.EMPNAME, C.STARTDATE;
 
---  ОПЕРАТОР CAST
---   11. Определить целую часть средних зарплат, по годам начисления.
+-- ОПЕРАТОР CAST
+-- 11. Определить целую часть средних зарплат, по годам начисления.
 SELECT YEAR, CAST(AVG(SALVALUE) AS NUMBER(10)) AS INTSALVALUE
     FROM SALARY
     GROUP BY YEAR
     ORDER BY YEAR;
 
---  ОПЕРАТОР CASE
---   12. Разделите сотрудников на возрастные группы: A) возраст 20-30 лет; B) 31-40 лет; C) 41-50; D) 51-60 или возраст не определён.
+-- ОПЕРАТОР CASE
+-- 12. Разделите сотрудников на возрастные группы: A) возраст 20-30 лет; B) 31-40 лет; C) 41-50; D) 51-60 или возраст не определён.
 SELECT EMPNO, EMPNAME,
     CASE
         WHEN MONTHS_BETWEEN (SYSDATE, BIRTHDATE) / 12 >= 20 AND MONTHS_BETWEEN (SYSDATE, BIRTHDATE) / 12 <= 30
@@ -126,17 +122,17 @@ SELECT EMPNO, EMPNAME,
     AS AGE_GROUP
     FROM EMP;
 
---   13. Перекодируйте номера отделов, добавив перед номером отдела буквы BI для номеров <=20,  буквы  LN для номеров >=30.
+-- 13. Перекодируйте номера отделов, добавив перед номером отдела буквы BI для номеров <=20, буквы LN для номеров >=30.
 SELECT D.DEPTNO,
     CASE
-        WHEN D.DEPTNO <= 20  THEN CONCAT('BI', CAST (D.DEPTNO AS VARCHAR(10)))
-        WHEN D.DEPTNO >= 30  THEN CONCAT('LN', CAST (D.DEPTNO AS VARCHAR(10)))
+        WHEN D.DEPTNO <= 20 THEN CONCAT('BI', CAST (D.DEPTNO AS VARCHAR(10)))
+        WHEN D.DEPTNO >= 30 THEN CONCAT('LN', CAST (D.DEPTNO AS VARCHAR(10)))
     END
     AS NEWDEPTNO, D.DEPTNAME, D.DEPTADDR
     FROM DEPT D
 
---  ОПЕРАТОР COALESCE (объединяться)
---   14. Выдать информацию о сотрудниках из таблицы EMP, заменив отсутствие данного о дате рождения датой '01-01-1000'.
+-- ОПЕРАТОР COALESCE (объединяться)
+-- 14. Выдать информацию о сотрудниках из таблицы EMP, заменив отсутствие данного о дате рождения датой '01-01-1000'.
 SELECT EMPNO, EMPNAME,
     COALESCE(BIRTHDATE, to_date('01-01-1000', 'dd-mm-yyyy')) AS BIRTHDATE
     FROM EMP
