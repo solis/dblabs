d4f59b1ef7b6ede14b9b7c4eb782e0dbeebf81d1
Hanna Rudakouskaya <hannarud@mail.ru>
2013-12-11 13:15:04 +0300

    Lab 06 19 finished - unsolved task 4, questions on behave tasks 2,7

--- lab06/lab0619.sql
+++ lab06/lab0619.sql
@@ -59,26 +59,143 @@ CREATE INDEX prepodavanie_prepod_predm_ind
 
 --4. В одну из таблиц добавьте поле (внешний ключ), значения которого ссылаются на поле – первичный ключ этой таблицы (academy.oracle.com\iLearning\2013-2014 Oracle Academy Database Programming with SQL – Student\Section 8 Working with DDL Statements). Составьте запросы на выборку данных с использованием рефлексивного соединения (academy.oracle.com\iLearning\2013-2014 Oracle Academy Database Programming with SQL – Student\Section 3 Executing Database Joins\Self Joins and Hierarchical Queries).
 
---Составьте запросы на выборку данных с использованием следующих операторов, конструкцийи функций языка SQL:
+--Составьте запросы на выборку данных с использованием следующих операторов, конструкций и функций языка SQL:
 --5. простого оператора CASE ();
---
+-- вывести список учебных предметов в формате Название предмета, Семестр преподавания, Экзамен(Нет экзамена), Зачет(Нет зачета) (словами)
+SELECT Nazvanie, Predmet_Semestr,
+    CASE Otchetnost_Ekzamen
+        WHEN '1' THEN 'Exam'
+        ELSE 'No exam'
+    END
+    AS Passing_An_Exam
+    CASE Otchetnost_Zachet
+        WHEN '1' THEN 'Test'
+        ELSE 'No test'
+    END
+    AS Passing_A_Test
+    FROM Uchebnyi_Predmet;
 
 --6. поискового оператора CASE();
+-- вывести список испытаний (экзаменов или зачетов), прошедших больше, чем год назад - ФИО сдававшего студента, название предмета, вид испытания и оценку
+SELECT s.FIO, p.Nazvanie,
+    CASE
+        WHEN i.Ekzamen_Ili_Zachet = '0' THEN 'Test'
+        ELSE 'Exam'
+    END
+    AS Passed_What,
+    CASE
+        WHEN i.Ekzamen_Ili_Zachet = '0' AND i.Ocenka=1 THEN 'Passed'
+        WHEN i.Ekzamen_Ili_Zachet = '0' AND i.Ocenka=0 THEN 'Failed'
+        ELSE CAST(i.Ocenka AS VARCHAR(2))
+    END
+    AS Ocenka
+    FROM Uchebnyi_Predmet p
+        NATURAL JOIN Ispytanie i
+        NATURAL JOIN Student s;
 
 --7. оператора WITH();
+-- выбрать имена преподавателей практики, ведущих предметы в осенних семестрах (1,3,5,7,9)
+--???????????????????????????????????
+WITH Osennie AS
+    (SELECT Predmet_ID
+        FROM Uchebnyi_Predmet
+        WHERE Predmet_Semestr IN (1,3,5,7,9))
+SELECT p.FIO
+    FROM Prepodavatel p NATURAL JOIN Prepodavanie pr
+    WHERE pr.Predmet_ID IN Osennie
+    GROUP BY p.FIO;
 
 --8. встроенного представления();
+-- выбрать имена преподавателей практики, ведущих предметы в осенних семестрах (1,3,5,7,9), и названия этих предметов
+SELECT p.FIO, o.Nazvanie
+    FROM Prepodavatel p
+    NATURAL JOIN Prepodavanie
+    NATURAL JOIN (SELECT Predmet_ID, Nazvanie FROM Uchebnyi_Predmet WHERE Predmet_Semestr IN (1,3,5,7,9)) o
+    GROUP BY p.FIO, o.Nazvanie;
 
 --9. некоррелированного запроса((academy.oracle.com\iLearning\2013-2014 Oracle Academy Database Programming with SQL – Student\Section 6 Creating Subqueries).);
+-- выбрать студентов, которые когда-либо сдавали экзамен
+SELECT *
+    FROM STUDENT
+    WHERE Student_ID IN
+        (SELECT Student_ID
+            FROM Ispytanie
+            WHERE Ekzamen_Ili_Zachet = '1')
+    ORDER BY FIO;
 
 --10. коррелированного запроса(academy.oracle.com\iLearning\2013-2014 Oracle Academy Database Programming with SQL – Student\Section 6 Creating Subqueries).);
+-- найти предметы, которые изучаются студентами
+SELECT p.Nazvanie
+    FROM Uchebnyi_Predmet p
+    WHERE p.Predmet_ID IN
+        (SELECT Predmet_ID
+            FROM Uchebnyi_Predmet
+            NATURAL JOIN Ispytanie
+            NATURAL JOIN Student
+            WHERE Semestr_Seichas = p.Predmet_Semestr);
 
 --11. функции NULLIF (academy.oracle.com\iLearning\2013-2014 Oracle Academy Database Programming with SQL – Student\Section 2 Using Single-Row Functions);
+-- вывести список всех студентов, убрав 5 семестр из процесса обучения
+SELECT Student_ID, Nomer_Zach_kn, FIO, NULLIF(Semestr_Seichas, 5) AS Semester(Not_5)
+    FROM Student;
 
 --12. функции NVL2 (academy.oracle.com\iLearning\2013-2014 Oracle Academy Database Programming with SQL – Student\Section 2 Using Single-Row Functions);
+-- попытаться идентифицировать студента хоть каким-то образом (если известен номер зачетной книжки - вывести его, иначе - вывести ФИО)
+SELECT Student_ID, NVL2(Nomer_zach_kn, Nomer_zach_kn, FIO) AS Best_Data_Available
+    FROM Student;
 
 --13. TOP-N анализа();
+-- вывести последних 25 студентов по номеру зачетной книжки
+SELECT *
+    FROM (SELECT *
+        FROM Student
+        ORDER BY Nomer_zach_kn DESC)
+    WHERE ROWNUM <= 25;
 
 --14. функции ROLLUP().
+-- подсчитать, каких оценок сколько было получено студентами
+SELECT Ocenka, COUNT(Ocenka) AS How_much
+    FROM Ispytanie
+    GROUP BY ROLLUP(Ocenka)
+    ORDER BY Ocenka;
 
 --15. Составьте запрос на использование оператора MERGE языка манипулирования данными.
+-- ввести 3 новых предмета, при этом если данные новых предметов совпадают со старыми, увеличить количество часов
+
+CREATE TABLE
+    Novyi_Predmet
+    (
+        Predmet_ID NUMBER(10) PRIMARY KEY,
+        Nazvanie VARCHAR2(50) NOT NULL,
+        Predmet_Semestr NUMBER(10) NOT NULL
+            CHECK (Predmet_Semestr > 0 AND Predmet_Semestr < 11),
+        Kolvo_Chasov NUMBER(10) NOT NULL,
+        Otchetnost_Ekzamen VARCHAR2(1) NOT NULL
+            CHECK (Otchetnost_Ekzamen IN('0', '1')), -- '0' - no exam, '1' - exam
+        Otchetnost_Zachet VARCHAR2(1) NOT NULL
+            CHECK (Otchetnost_Zachet IN('0', '1')) -- '0' - no test, '1' - test
+    );
+
+INSERT INTO Novyi_Predmet VALUES
+    (1, 'Математический анализ', 1, 22, 1, 1);
+
+INSERT INTO Novyi_Predmet VALUES
+    (2, 'Политология', 5, 10, 0, 1);
+
+INSERT INTO Novyi_Predmet VALUES
+    (3, 'Физкультура', 1, 68, 1, 1);
+
+INSERT INTO Novyi_Predmet VALUES
+    (4, 'Матричный анализ', 5, 34, 0, 1);
+
+MERGE INTO Uchebnyi_Predmet up
+    USING (SELECT *
+        FROM Novyi_Predmet) o
+        ON (up.Predmet_ID = o.Predmet_ID)
+    WHEN MATCHED THEN
+        UPDATE SET up.Kolvo_Chasov = up.Kolvo_Chasov + o.Kolvo_Chasov
+    WHEN NOT MATCHED THEN
+        INSERT (up.Predmet_ID, up.Nazvanie, up.Predmet_Semestr, up.Kolvo_Chasov, up.Otchetnost_Ekzamen, up.Otchetnost_Zachet)
+        VALUES (o.Predmet_ID, o.Nazvanie, o.Predmet_Semestr, o.Kolvo_Chasov, o.Otchetnost_Ekzamen, o.Otchetnost_Zachet);
+         
+SELECT * FROM Uchebnyi_Predmet;
