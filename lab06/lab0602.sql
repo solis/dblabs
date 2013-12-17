6c76c8e6bad97a551313d0261d9796dbab47d9d8
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2013-12-18 01:06:08 +0300

    Encoding fixes :)

--- lab06/lab0602.sql
+++ lab06/lab0602.sql
@@ -161,7 +161,7 @@ INSERT INTO HIPPODROME VALUES
 
 
 --3. Создайте индексы для тех полей базы данных, для которых это необходимо
-CREATE INDEX JOCKEY_NAME ON JOCKEYS(ID, NAME)
+CREATE INDEX JOCKEY_NAME ON JOCKEYS(ADDRESS, NAME)
 
 
 --4. В одну из таблиц добавьте поле (внешний ключ), значения которого ссылаются на поле –
@@ -260,3 +260,36 @@ SELECT HORSE_ID, POSITION, COUNT(POSITION)
     GROUP BY ROLLUP(HORSE_ID, POSITION)
 
 --15. Составьте запрос на использование оператора MERGE языка манипулирования данными.
+
+CREATE TABLE
+    NEW_RaceAreas
+    (
+        RaceArea_ID NUMBER(10) PRIMARY KEY,
+        RaceArea_Address VARCHAR2(300) NOT NULL,
+        RaceArea_MaxNumOfGuests NUMBER(10) NOT NULL
+            CHECK (RaceArea_MaxNumOfGuests > 0),
+        RaceArea_Square NUMBER(10) --in hectares
+            CHECK (RaceArea_Square > 0)
+    );
+
+INSERT INTO NEW_RaceAreas VALUES
+    (1, 'Knighton, Powys LD7 1DL, United Kingdom', 3000, 5000);
+
+INSERT INTO NEW_RaceAreas VALUES
+    (2, 'Ketley Bank, Telford, Telford and Wrekin TF2 0EB, United Kingdom', 12000, 11000);
+
+INSERT INTO NEW_RaceAreas VALUES
+    (3, '85 Llanelian Rd, Old Colwyn, Colwyn Bay, Conwy LL29 8UN, Great Bitain', 3000, 4000);
+
+MERGE INTO RaceAreas ra
+    USING (SELECT *
+        FROM NEW_RaceAreas) newra
+        ON (ra.RaceArea_Address = newra.RaceArea_Address)
+    WHEN MATCHED THEN
+        UPDATE SET ra.RaceArea_MaxNumOfGuests = newra.RaceArea_MaxNumOfGuests
+        AND ra.RaceArea_Square = newra.RaceArea_Square
+    WHEN NOT MATCHED THEN
+        INSERT (ra.RaceArea_ID, ra.RaceArea_Address, ra.RaceArea_MaxNumOfGuests, ra.RaceArea_Square)
+        VALUES (RaceArea_ID_Generator.NEXTVAL, newra.RaceArea_Address, newra.RaceArea_MaxNumOfGuests, newra.RaceArea_Square);
+
+SELECT * FROM RaceAreas;
